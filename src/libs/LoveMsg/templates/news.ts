b9fa032078cdfa4b54d4b1888e50aa513eb450c2
zccl <570399406@qq.com>
2022-01-28 10:11:10 +0800

    测试

--- src/libs/LoveMsg/templates/news.ts
+++ src/libs/LoveMsg/templates/news.ts
@@ -25,7 +25,9 @@ export const newsTemplate = (list: TodayHeadlines[]) => {
     articles.unshift({
       title: '摸鱼日历',
       description: '摸鱼日记的描述',
-      url: 'https://api.vvhan.com/api/lolskin',
+      // url: 'https://api.vvhan.com/api/lolsk0in',//lol皮肤
+      // url: 'https://api.vvhan.com/api/avatar', //各种头像
+      url: 'https://api.vvhan.com/api/moyu', // 摸鱼日历图
       picurl: 'https://i.bmp.ovh/imgs/2022/01/03ce2238ad19eb4d.png',
     })
   }
